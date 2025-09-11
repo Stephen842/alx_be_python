890e71a6c282709d3bc671e586d41181ff93ea5c
user.stephen842 <ugotachisomstephen@gmail.com>
2025-09-11 21:30:58 +0100

    Did some hotfixing the code

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -4,10 +4,10 @@ number1 = 10
 number2 = 5
 
 addi = number1 + number2
-subt = number2 - number1
+subt = number1 - number2
 mult = number1 * number2
 
 print(f'Addition of {number1} and {number2} is {addi}')
-print(f'Subtraction of {number2} and {number1} is {subt}')
+print(f'Subtraction of {number1} and {number2} is {subt}')
 print(f'Multiplication of {number1} and {number2} is {mult}')
 

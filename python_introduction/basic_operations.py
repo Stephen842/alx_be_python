6570db7f532fa540e21a1b3e43936689cd9ce098
stephen842 <ugotachisomstephen@gmail.com>
2025-09-11 21:00:17 +0100

    Created a directory for storing all the introduction python program

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+## This is a basic program that perform basic arithmetics
+
+number1 = 10
+number2 = 5
+
+addi = number1 + number2
+subt = number2 - number1
+mult = number1 * number2
+
+print(f'Addition of {number1} and {number2} is {addi}')
+print(f'Subtraction of {number2} and {number1} is {subt}')
+print(f'Multiplication of {number1} and {number2} is {mult}')
+

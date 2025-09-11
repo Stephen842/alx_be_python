d4cc947ed7d2ab5c9dfb92923a59f95e2057b872
user.stephen842 <ugotachisomstephen@gmail.com>
2025-09-11 21:42:31 +0100

    created a program that does calculation on simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+## This is a python program to calculate simple interest
+
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = principal * rate * time
+
+print(f'The simple interest is: {interest}')

bc03b98879da3953b34fbd1e0c18a0a404721df3
user.stephen842 <ugotachisomstephen@gmail.com>
2025-09-11 23:11:18 +0100

    This program calculate the future age of individuals

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+## This program calculate the future age difference of user in year 2050
+
+age = int(input('How old are you?'))
+current_year = 2050 - 2023
+
+future_age = age + current_year
+
+print(f'In 2050, you will be {future_age} years old.')
